faf250337c157e8b6388caa6b73366a40dd447ea
pengo-sudo <pogopengo@gmail.com>
2026-07-26 09:55:26 +0200

    Create PluginInfo.cs

---
+++ Diros.Plugin/PluginInfo.cs
@@ -0,0 +1,10 @@
+namespace Diros.Plugin;
+
+public static class PluginInfo
+{
+	public const string GUID = "com.diros.gorillatag.diros";
+
+	public const string Name = "Diros Gorilla Tag Mod";
+
+	public const string Version = "1.0.0";
+}
